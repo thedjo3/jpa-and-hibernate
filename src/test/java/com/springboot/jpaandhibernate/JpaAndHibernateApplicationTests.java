9204e3545c5971124062be04083c1d25b44efe1e
Djan Daudov <djandaudovv@gmail.com>
2024-02-21 22:46:32 +0200

    Initial commit

---
+++ src/test/java/com/springboot/jpaandhibernate/JpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
