471363cb1c1f38e019756d7884077b9e5f792e9d
Djan Daudov <djandaudovv@gmail.com>
2024-02-24 22:53:21 +0200

    Interting and Deleting Data with Command Line Runner

--- src/main/java/com/springboot/jpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/springboot/jpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.springboot.jpaandhibernate.course.jdbc;
 
+import com.springboot.jpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -15,11 +16,20 @@ public class CourseJdbcRepository {
     private static String INTERT_QUERY =
             """
                 insert into course (id, name, author)
-                values(1, 'Learn AWS', 'in28minutes');
+                values(?, ?, ?);
+            """;
+    private static String DELETE_QUERY =
+            """
+                delete from course
+                where id = ?
             """;
 
-    public void insert() {
-        springJdbcTemplate.update(INTERT_QUERY);
+    public void insert(Course course) {
+        springJdbcTemplate.update(INTERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+        //springJdbcTemplate.update(DELETE_QUERY, course.getId());
     }
 
+    public void delete(long id) {
+        springJdbcTemplate.update(DELETE_QUERY, id);
+    }
 }
